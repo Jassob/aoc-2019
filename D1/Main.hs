7fa5e52dd06ed1257ae6c92fc02a683638e35918
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2019-12-01 12:22:27 +0100

    chore[Day 1]: Scaffolding

--- D1/Main.hs
+++ D1/Main.hs
@@ -1,24 +1,54 @@
 module Main where
 
 import           Data.List                      ( foldl' )
+import           System.Environment             ( getArgs )
 
 import           Types
 
 readInput :: String -> [Mass]
 readInput = map (Mass . read) . lines
 
-solvePart1 :: [Mass] -> Int
-solvePart1 = getFuel . foldl' (+) 0 . map calculateFuel
+part1 :: [Mass] -> Int
+part1 = getFuel . foldl' (+) 0 . map calculateFuel
+
+part2 :: [Mass] -> Int
+part2 = undefined
 
 main :: IO ()
 main = do
-  inp <- readInput <$> getContents
-  print $ solvePart1 inp
+  args <- getArgs
+  case args of
+    ("test" : _) | tests     -> putStrLn "All tests passed!"
+                 | otherwise -> putStrLn "At least one test failed"
+    ("part1" : _) -> solve part1
+    ("part2" : _) -> solve part2
+    ("help" : _) -> printHelp
+    [] -> solve part1 -- default is to solve part1
+    _  -> printHelp
+  where
+    solve :: ([Mass] -> Int) -> IO ()
+    solve f = print =<< f . readInput <$> getContents
+
+    printHelp :: IO ()
+    printHelp = putStrLn $ unlines
+      [ "Usage: Day1 [OPTION], if no option is given part1 is assumed"
+      , "For both part1 and part2 input is supposed to be given on standard input"
+      , "redirecting from a file is recommended"
+      , ""
+      , "Valid options:"
+      , "test\t\tRun test suite"
+      , "part1\t\tSolve part 1"
+      , "part2\t\tSolve part 2"
+      , "help\t\tPrint this text"
+      ]
 
 tests :: Bool
-tests = all (uncurry (==))
-  [ (calculateFuel (Mass 12), 2)
-  , (calculateFuel (Mass 14), 2)
-  , (calculateFuel (Mass 1969), 654)
-  , (calculateFuel (Mass 100765), 33586)
-  ]
+tests =
+  all
+      (uncurry (==))
+      [ (calculateFuel (Mass 12)    , 2)
+      , (calculateFuel (Mass 14)    , 2)
+      , (calculateFuel (Mass 1969)  , 654)
+      , (calculateFuel (Mass 100765), 33586)
+      ]
+    && (part1 (map Mass [12, 14, 1969, 100765]) == 34244)
