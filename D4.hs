76e512c52e535fda9fad220780e95e9c16268204
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2019-12-04 10:49:28 +0100

    feat[Day 4]: Solve part 2

--- D4.hs
+++ D4.hs
@@ -5,10 +5,6 @@ import           Data.List                      ( sort
                                                 , group
                                                 )
 
-generatePasswords :: Int -> Int -> [Int]
-generatePasswords min' max' =
-  [ pwd | pwd <- [min' .. max'], validPassword (show pwd) ]
-
 validPassword :: String -> Bool
 validPassword pwd = length pwd == 6 && allIncreasing pwd && doubleLetters pwd
  where
@@ -18,11 +14,27 @@ validPassword pwd = length pwd == 6 && allIncreasing pwd && doubleLetters pwd
   doubleLetters :: String -> Bool
   doubleLetters s = foldr (\l b -> b || length l > 1) False $ group s
 
-test :: Bool
-test = validPassword "111111" && not (validPassword "223450") && not
+part1test :: Bool
+part1test = validPassword "111111" && not (validPassword "223450") && not
   (validPassword "123789")
+
+validPart2 :: String -> Bool
+validPart2 pwd = length pwd == 6 && allIncreasing pwd && doubleLetters pwd
+  where
+    allIncreasing :: String -> Bool
+    allIncreasing s = sort s == s
+
+    doubleLetters :: String -> Bool
+    doubleLetters s = foldr (\l b -> b || length l == 2) False $ group s
+
+part2test :: Bool
+part2test = validPart2 "112233" && not (validPart2 "123444") && validPart2 "111122"
 
 main :: IO ()
 main = do
   putStrLn "Part 1"
-  putStrLn $ "Answer: " <> (show . length $ generatePasswords 284639 748759)
+  putStrLn $ "Answer: " <> (show . length $ [ pwd | pwd <- [min' .. max'], validPassword (show pwd) ])
+  putStrLn "Part 2"
+  putStrLn $ "Answer: " <> (show . length $ [ pwd | pwd <- [min' .. max'], validPart2 (show pwd)])
+  where min' = 284639 :: Integer
+        max' = 748759 :: Integer
