25577935767d417c0685c117788b563d442e1657
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2019-12-04 10:07:57 +0100

    chore[Day 1]: Inline Types module

--- D1/Main.hs
+++ D1/Main.hs
@@ -1,9 +1,20 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Main where
 
 import           Data.List                      ( unfoldr )
 import           System.Environment             ( getArgs )
 
-import           Types
+newtype Mass = Mass { getMass :: Int }
+  deriving (Eq, Show)
+
+newtype Fuel = Fuel { getFuel :: Int }
+  deriving (Eq, Num)
+
+instance Show Fuel where
+  show (Fuel i) = show i
+
+calculateFuel :: Mass -> Fuel
+calculateFuel (Mass m) = Fuel $ m `div` 3 - 2
 
 readInput :: String -> [Mass]
 readInput = map (Mass . read) . lines
